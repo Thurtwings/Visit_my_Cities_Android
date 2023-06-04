83d333471bb0e80dc6e5452c7ba2332affc52f83
Thurtwings <thurtwings@gmail.com>
2023-06-04 19:06:14 +0200

    ADDED nouvelles pages, refacto map pour avoir la position locale de l'utilisateur. WIP: none. TODO: Trello, mise au point visuels, Chaier des charges, powerpoint

---
+++ app/src/main/java/com/example/visit_my_cities_android/MapScreen.java
@@ -0,0 +1,89 @@
+package com.example.visit_my_cities_android;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapScreen extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final int FINE_PERMISSION_CODE = 1;
+    private GoogleMap mainMap;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_display);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        GetLastLocation();
+
+    }
+
+    private void GetLastLocation()
+    {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null)
+                {
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MapScreen.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+        mainMap = googleMap;
+
+        LatLng user = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        mainMap.addMarker(new MarkerOptions().position(user).title("Ma position"));
+        mainMap.moveCamera(CameraUpdateFactory.newLatLng(user));
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == FINE_PERMISSION_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                GetLastLocation();
+            }
+            else
+            {
+                Toast.makeText(this, "Permission d'acceder a la localisation refusée, merci de l'autoriser", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/visit_my_cities_android/UserProfileScreen.java
+++ app/src/main/java/com/example/visit_my_cities_android/UserProfileScreen.java
@@ -1,4 +1,62 @@
 package com.example.visit_my_cities_android;
 
-public class UserProfileScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+
+public class UserProfileScreen extends AppCompatActivity
+{
+
+
+
+    Button feedButton;
+    Button listsButton;
+    ImageView userPicture;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_screen);
+
+        userPicture = findViewById(R.id.UserPicture);
+        feedButton = (Button) findViewById(R.id.UPFeedButton);
+        listsButton = (Button) findViewById(R.id.UPListButton);
+
+        feedButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                GoToFeed();
+            }
+        });
+
+        listsButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                GoToLists();
+            }
+        });
+
+    }
+
+    private void GoToLists()
+    {
+        Intent nav = new Intent(this, ListsScreen.class);
+
+        startActivity(nav);
+    }
+
+    private void GoToFeed()
+    {
+        Intent nav = new Intent(this, FeedScreen.class);
+        startActivity(nav);
+    }
 }
