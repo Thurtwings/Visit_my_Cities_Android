8eb910ea38ffe92e009ad78c219cf8905c341ff4
ZefitaRo <gregory.schneider@uha.fr>
2023-06-08 16:25:45 +0200

    Commit du desespoir

--- app/src/main/java/com/example/visit_my_cities_android/LoginScreen.java
+++ app/src/main/java/com/example/visit_my_cities_android/LoginScreen.java
@@ -4,34 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.example.visit_my_cities_android.ConnexionServer.Utilisateur.ConnexionServerGetUsers;
+
 public class LoginScreen extends AppCompatActivity
 {
 
-    private ImageView background4;
-    private ImageView banniere4;
-    private ImageView imageNavbar4;
+    private ImageView background_LoginScreen;
+    private ImageView banniere_LoginScreen;
+    private ImageView imageNavbar_LoginScreen;
 
-    private TextView textViewPseudo;
-    private TextView textViewPassword;
+    private TextView textViewPseudo_LoginScreen;
+    private TextView textViewPassword_LoginScreen;
 
     private EditText editTextPseudo;
     private EditText editTextPassword;
 
-    private ImageButton SignupScreenButton;
-    private ImageButton UserScreenButton;
+    private ImageButton SignupScreenButton_LoginScreen;
+    private ImageButton UserScreenButton_LoginScreen;
 
-    private ImageButton homeButton4;
-    private ImageButton mapButton4;
-    private ImageButton buildingButton4;
-    private ImageButton addButton4;
-    private ImageButton accountButton4;
+    private ImageButton homeButton_LoginScreen;
+    private ImageButton mapButton_LoginScreen;
+    private ImageButton buildingButton_LoginScreen;
+    private ImageButton addButton_LoginScreen;
+    private ImageButton accountButton_LoginScreen;
+
+    private DBManager databaseManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,64 +55,64 @@ public class LoginScreen extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        background4 = (ImageView) findViewById(R.id.background4);
-        banniere4 = (ImageView) findViewById(R.id.banniere4);
-        imageNavbar4 = (ImageView) findViewById(R.id.imageNavbar4);
+        background_LoginScreen = (ImageView) findViewById(R.id.background4);
+        banniere_LoginScreen = (ImageView) findViewById(R.id.banniere4);
+        imageNavbar_LoginScreen = (ImageView) findViewById(R.id.imageNavbar4);
 
 
-        textViewPseudo = (TextView) findViewById(R.id.textViewPseudo);
-        textViewPassword = (TextView) findViewById(R.id.textViewPassword);
+        textViewPseudo_LoginScreen = (EditText) findViewById(R.id.editTextPseudo);
+        textViewPassword_LoginScreen = (EditText) findViewById(R.id.editTextPassword);
 
 
-        SignupScreenButton = (ImageButton) findViewById(R.id.SignupScreenButton);
-        UserScreenButton = (ImageButton) findViewById(R.id.UserScreenButton);
+        SignupScreenButton_LoginScreen = (ImageButton) findViewById(R.id.SignupScreenButton);
+        UserScreenButton_LoginScreen = (ImageButton) findViewById(R.id.UserScreenButton);
 
 
-        homeButton4 = (ImageButton) findViewById(R.id.homeButton4);
-        mapButton4 = (ImageButton) findViewById(R.id.mapButton4);
-        buildingButton4 = (ImageButton) findViewById(R.id.buildingButton4);
-        addButton4 = (ImageButton) findViewById(R.id.addButton4);
-        accountButton4 = (ImageButton) findViewById(R.id.accountButton4);
-
-        homeButton4.setOnClickListener(new View.OnClickListener() {
+        homeButton_LoginScreen = (ImageButton) findViewById(R.id.homeButton4);
+        mapButton_LoginScreen = (ImageButton) findViewById(R.id.mapButton4);
+        buildingButton_LoginScreen = (ImageButton) findViewById(R.id.buildingButton4);
+        addButton_LoginScreen = (ImageButton) findViewById(R.id.addButton4);
+        accountButton_LoginScreen = (ImageButton) findViewById(R.id.accountButton4);
+        databaseManager = new DBManager(getApplicationContext());
+        homeButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GoToHomeScreen();
             }
         });
 
-        mapButton4.setOnClickListener(new View.OnClickListener() {
+        mapButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GoToMapScreen();
             }
         });
 
-        buildingButton4.setOnClickListener(new View.OnClickListener() {
+        buildingButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GoToListsScreen();
             }
         });
 
-       addButton4.setOnClickListener(new View.OnClickListener() {
+        addButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GoToFeedScreen();
             }
         });
 
-       SignupScreenButton.setOnClickListener(new View.OnClickListener() {
+        SignupScreenButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 GoToSignUp();
             }
         });
 
-        UserScreenButton.setOnClickListener(new View.OnClickListener() {
+        UserScreenButton_LoginScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                GoToProfile();
+                connectUser();
             }
         });
     }
@@ -137,4 +153,46 @@ public class LoginScreen extends AppCompatActivity
         Intent nav = new Intent(this, ListsScreen.class);
         startActivity(nav);
     }
+
+
+
+    public void connectUser() {
+        String url = "http://jdevalik.fr/api/VMC_PHP_SBG/get_pass.php";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("pseudo", textViewPseudo_LoginScreen.getText().toString());
+        params.put("pass", textViewPassword_LoginScreen.getText().toString());
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    boolean success = response.getBoolean("success");
+
+                    if (success) {
+                        // Login successful
+                        Toast.makeText(getApplicationContext(), "Connexion réussie", Toast.LENGTH_LONG).show();
+                        Intent nav = new Intent(LoginScreen.this, UserProfileScreen.class);
+                        startActivity(nav);
+                    } else {
+                        // Login failed
+                        Toast.makeText(getApplicationContext(), "Pseudo ou mot de passe incorrect", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Erreur de réponse du serveur", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Erreur de connexion", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        databaseManager.queue.add(jsonObjectRequest);
+    }
+
+
 }
